bb43fc66f90538d868a27fb7e14bca71550d88a9
Chetan Gandhi <69512667+Chetan2808@users.noreply.github.com>
2022-04-23 17:20:16 +0530

    Add files via upload

---
+++ inverted full pyramid.cpp
@@ -0,0 +1,20 @@
+//inverted full pyramid
+#include <iostream>
+using namespace std;
+int main() {
+   int n;
+   cin>>n;
+   for(int i = n ; i>=1 ; i--){
+       //spaces
+       for(int j=1;j<=(n-i);j++){
+           cout<<" ";
+       }
+       //stars
+       for(int j = 1 ;j<=i;j++){
+           cout<<"* ";
+       }
+       cout<<endl;
+   }
+
+    return 0;
+}
