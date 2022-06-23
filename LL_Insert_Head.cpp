79124fd356a20d15c2801bd0677e5f86a37ef75e
Chetan Gandhi <69512667+Chetan2808@users.noreply.github.com>
2022-06-23 23:17:37 +0530

    Update LL_Insert_Head.cpp

--- LL_Insert_Head.cpp
+++ LL_Insert_Head.cpp
@@ -1,34 +1,36 @@
-insert at head
-#include iostream
+
+//insert at head
+#include <iostream>
 
 using namespace std;
 class Node{
-    public
+    public:
     int data;
-    Node next;
+    Node* next;
     
     Node(int d){
-        this-data=d;
-        this-next=NULL;
+        this->data=d;
+        this->next=NULL;
         
     }
 };
-void insertAthead(Node &head,int d){
-    step1 create
-    Node newNode = new Node(d);
-    step2 set the pointer to head node
-    newNode - next = head;
-    step3update head
+void insertAthead(Node* &head,int d){
+    //step1 create
+    Node* newNode = new Node(d);
+    //step2 set the pointer to head node
+    newNode -> next = head;
+    //step3update head
     head= newNode;
 }
 
 int main()
 {
-    Node first = new Node(5);
-    we created this head below so that the above first node pointer and the below head point to the same node 
-    Node head = first;
+    Node* first = new Node(5);
+    //we created this head below so that the above first node pointer and the below head point to the same node 
+    Node* head = first;
     insertAthead(head,6);
-    couthead- data     first-dataendl;
-    couthead-next  first-nextendl;
+    cout<<head-> data<<" "<<first->data<<endl;
+    cout<<head->next<< " " <<first->next<<endl;
     return 0;
 }
+
