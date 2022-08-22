3a22bf8a1d9880b519f6cdde1e32bceff1395a67
Chetan Gandhi <69512667+Chetan2808@users.noreply.github.com>
2022-08-22 15:58:39 +0530

    Add files via upload

---
+++ recursion_reverse.cpp
@@ -0,0 +1,16 @@
+//Code to print every number till n
+#include <iostream>
+using namespace std;
+int reverse(int n){
+    if(n==0){
+        return 0;
+    }
+    cout<<n<<endl;
+    return reverse(n-1);
+}
+int main() {
+    // Write C++ code here
+    
+    reverse(24);
+    return 0;
+}
