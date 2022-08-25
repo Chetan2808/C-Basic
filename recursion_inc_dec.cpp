f5cb16ca3263a7c35275e9d9f6d95561198f7815
Chetan Gandhi <69512667+Chetan2808@users.noreply.github.com>
2022-08-25 10:31:00 +0530

    Add files via upload

---
+++ recursion_inc_dec.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int reverse(int n){
+    
+    if(n==0){
+        return 0;
+    }
+    cout<<n<<" ";
+    reverse(n-1);
+    
+}
+void front(int n){
+    
+    if(n==0){
+        return;
+    }
+    
+    front(n-1);
+    cout<<n<<" ";
+}
+int main() {
+    // Write C++ code here
+    int n;
+    cout<<":Enter the number: ";
+    cin>>n;
+    cout<<"Printing in Decreasing and increasing order"<<endl;
+    
+    reverse(n);
+    front(n);
+    
+   
+    return 0;
+}
